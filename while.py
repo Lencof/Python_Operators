7a58b3f7e923fc576fe0e3871e6fa3bc694167ec
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-10 13:45:55 +0300

    Update while.py

--- while.py
+++ while.py
@@ -1,2 +1,18 @@
 # __Author__ __Lencof__
 # while.py
+
+number = 40 # your figures 
+running = True # True
+
+while running:
+         guess - int(input('Please enter an integer :')) # your text
+    
+         if guess == number: # check guess == number
+                 print('Congratulations!') # your text
+                 running = False # False
+         elif guess < number: # guess < number 
+                 print('No, hidden number is slightly larger than that.') # your text
+         else:
+                 print('No, the hidden number is slightly larger than that.') # your text
+else:
+         print('The cycle is over for now.') # your text
