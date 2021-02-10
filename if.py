0de62c6957734b68a43d40d2ee7c80cacc073644
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-10 13:26:46 +0300

    Update if.py

--- if.py
+++ if.py
@@ -1,16 +1,16 @@
 # __Author__ __Lencof__
 # if.py
 
-number = 70
-guess = int(imput('Введите целое число :'))
+number = 70 # you figures 
+guess = int(imput('Введите целое число :')) # your text
 
-if guess == number:
-    print('Поздравляю, вы угадали.')
-    print('Хотя и не выграли никакого приза!')
-elif guess < number:
-    print('Нет загаданное число немного больше этого.')
+if guess == number: # check if they are both the same
+    print('Поздравляю, вы угадали.') # your text
+    print('Хотя и не выграли никакого приза!') # your text
+elif guess < number: # number more
+    print('Нет загаданное число немного больше этого.') # your text
     
-else:
-    print('Нет, загаданное число немного больше этого .')
+else: 
+    print('Нет, загаданное число немного больше этого .') # your text
   
-print('Завершено')
+print('Завершено') # your text
