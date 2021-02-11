02dbb936b4ec70f87e294c35a0c59f364f96545d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-11 08:15:24 +0300

    Update for.py

--- for.py
+++ for.py
@@ -5,3 +5,11 @@ for i in range(1, 5): # your figures
   print(i) # print(i) output i
  else:
   print('For loop ended') # your text
+
+  # $ python for.py
+  '''1
+  2
+  3
+  4
+  5
+  For loop ended'''
