7f4cd8456df6406d6320bcbbeef488050db3bba2
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-11 08:14:08 +0300

    Update for.py

--- for.py
+++ for.py
@@ -1,7 +1,7 @@
 # __Author__ __Lencof__
 # for.py
 
-for i in range(1, 5):
-  print(i)
+for i in range(1, 5): # your figures
+  print(i) # print(i) output i
  else:
-  print('For loop ended')
+  print('For loop ended') # your text
