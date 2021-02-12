851ad624b4cad939e259d0fb19f7a31eed815247
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-12 09:12:46 +0300

    Update break.py

--- break.py
+++ break.py
@@ -1,2 +1,11 @@
 # __Author__ __Lencof__
 # break.py
+
+while True:
+    a = input('Enter anything :' )
+    if s == 'exit':
+        break
+    print('Line length: ', len(s))
+print('Completion')
+
+# $ python break.py
