efa3685fa5a51eee3c2cb5719f2cf71191c7e954
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-12 10:37:20 +0300

    Update continue.py

--- continue.py
+++ continue.py
@@ -1,2 +1,11 @@
 # __Author__ __Lencof__
 # continue.py
+
+while True:
+    s = input('Enter anything : ')
+    if s == 'exit':
+        break
+    if len(s) < 3:
+        print('Too few')
+        continue
+    print('Enterrd string of sufficient lenght')
